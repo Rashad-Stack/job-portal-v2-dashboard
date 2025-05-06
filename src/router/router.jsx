0d8f2c6eb40789c2081e922b04ffd5d2f0cb883c
Sayed Razibur Rahman <sayedrazibur@gmail.com>
2025-05-06 10:29:02 +0600

    u

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -1,6 +1,5 @@
 import { Routes, Route, Navigate, BrowserRouter } from 'react-router'; // use 'react-router-dom'
 
-<<<<<<< HEAD
 import DashboardLayout from '../layout/DashboardLayout';
 import Home from '../pages/Home/Home';
 import Login from '../pages/Auth/Login';
@@ -16,23 +15,6 @@ import PrivateRoute from './PrivateRoute.jsx';
 import Status from '../pages/jobIndex/Status.jsx';
 import Category from '../pages/jobIndex/Category.jsx';
 import Manage from '../pages/jobIndex/ManageJobIndex.jsx';
-=======
-import DashboardLayout from "../layout/DashboardLayout.jsx";
-import Home from "../pages/Home/Home.jsx";
-import Login from "../pages/Auth/Login.jsx";
-import Register from "../pages/Register/Register.jsx";
-import Profile from "../pages/Profile/Profile.jsx";
-import ChangePassword from "../pages/Password/ChangePassword.jsx";
-import JobCreate from "../pages/Jobs/JobCreate.jsx";
-import AllJobs from "../pages/Jobs/AllJobs.jsx";
-import JobEdit from "../pages/Jobs/JobEdit.jsx";
-import JobView from "../pages/Jobs/JobView.jsx";
-import Moderators from "../pages/Auth/Moderators/Moderators.jsx";
-import PrivateRoute from "./PrivectRoute.jsx";
-import Status from "../pages/jobIndex/Status.jsx";
-import Category from "../pages/jobIndex/Category.jsx";
-import Manage from "../pages/jobIndex/ManageJobIndex.jsx";
->>>>>>> 9c9bcb7381188a4f71b4f93d8351d62aba394816
 
 function Router() {
   return (
