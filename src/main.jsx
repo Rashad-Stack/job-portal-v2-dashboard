44e6c737da55735f05e56fc5822881ad31f62792
joha <johahossainm@gmail.com>
2025-05-06 10:28:42 +0600

    kk

--- src/main.jsx
+++ src/main.jsx
@@ -1,31 +1,14 @@
-<<<<<<< HEAD
-import React, { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App';
+import React, { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import Router from "./router/router";
 
-import './index.css';
-import '@fontsource/poppins';
-import '@fontsource/urbanist';
-import '@fontsource/manrope';
+import "./index.css";
+import "@fontsource/poppins";
+import "@fontsource/urbanist";
+import "@fontsource/manrope";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <Router />
   </StrictMode>
 );
-=======
-import React, { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import Router from "./router/router"; 
-
-import './index.css';
-import '@fontsource/poppins';
-import '@fontsource/urbanist';
-import '@fontsource/manrope';
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <Router /> 
-  </StrictMode>
-);
->>>>>>> 9c9bcb7381188a4f71b4f93d8351d62aba394816
