3c474312a8112b9e068564f7114447a0db0a5c9f
Rashad-Stack <tex.eng.rashed@gmail.com>
2025-05-18 05:06:00 +0600

    Refactor API configuration and enhance axios instance with request/response interceptors

--- src/api/axios/applications.js
+++ src/api/axios/applications.js
@@ -1,25 +1,11 @@
 import axiosInstance from ".";
 
 const API_BASE_URL = "http://localhost:3000/api/v2/job/application";
-const getAuthHeaders = () => {
-  const token = localStorage.getItem("svaAuth");
-  if (!token) {
-    throw new Error("Authentication token not found. Please log in again.");
-  }
-  return {
-    "Content-Type": "application/json",
-    Authorization: `Bearer ${token}`,
-  };
-};
 
 // Get all jobs
 export const getAllApplications = async () => {
   try {
-    const response = await axiosInstance.get(`${API_BASE_URL}/all`, {
-      method: "GET ",
-      headers: getAuthHeaders(),
-      credentials: "include",
-    });
+    const response = await axiosInstance.get("all");
     if (!response.ok) {
       throw new Error("Failed to fetch jobs");
     }
--- src/api/axios/index.js
+++ src/api/axios/index.js
@@ -5,6 +5,31 @@ const axiosInstance = axios.create({
   headers: {
     "Content-Type": "application/json",
   },
+  withCredentials: true,
+  credentials: "include",
 });
+
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("svaAuth");
+    if (!token) {
+      throw new Error("Authentication token not found. Please log in again.");
+    }
+    config.headers.Authorization = `Bearer ${token}`;
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+axiosInstance.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
 
 export default axiosInstance;
