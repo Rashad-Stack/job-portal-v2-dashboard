91947dd83b8b7491aacc78d0a93034ecfba1fbc3
mehedi8430 <mehedialhasan1999@gmail.com>
2025-05-19 05:25:45 +0600

    feat: add sonner for toast notifications and improve form validation in CreateForms

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
+import { Toaster } from "sonner";
 import "./App.css";
 import Router from "./router/router";
 
 function App() {
   // return <RouterProvider router={router} />;
-  return <Router />;
+  return (
+    <>
+    <Toaster position="top-center" richColors />
+    <Router />
+    </>
+  );
 }
 
 export default App;
